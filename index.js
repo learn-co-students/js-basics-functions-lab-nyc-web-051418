89e123ac9d578d151855340c07b39d1a41eaa277
Ledio <L.Bode515@gmail.com>
2018-06-25 21:00:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(randVal){
+  if (randVal < 42) {
+    return (42 - randVal);
+  } else {
+    return (randVal - 42);
+  }
+}
+
+function distanceFromHqInFeet(randVal){
+  return (distanceFromHqInBlocks(randVal) * 264);
+}
+
+function distanceTravelledInFeet(aVal,bVal){
+  if (aVal < bVal){
+    return ((bVal - aVal) * 264);
+  } else {
+    return ((aVal - bVal) * 264);
+  }
+}
+
+function calculatesFarePrice(start, end){
+
+const foot = distanceTravelledInFeet(start,end)
+  if (foot <= 400) {
+    return 0;
+  } else if (foot > 400 && foot <= 2000) {
+     return ((foot-400) * .02);
+  } else if (foot > 2000 && foot < 2500) {
+    return 25;
+    } else {
+    return 'cannot travel that far';
+  }
+}
